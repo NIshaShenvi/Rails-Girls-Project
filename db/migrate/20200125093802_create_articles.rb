5b6ad305acf7aee473550f1de9db783bc9b811e6
Nisha <shenvinisha123@gmail.com>
2020-01-25 15:20:53 +0530

    Initial setu[ of project

---
+++ db/migrate/20200125093802_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[5.1]
+  def change
+    create_table :articles do |t|
+      t.text :content
+      t.integer :likes
+      t.integer :dislike
+
+      t.timestamps
+    end
+  end
+end
